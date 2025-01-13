5b5a60de9871ffa49026d0f025520085f21d92ed
Calvin96C <calvinfueller92@gmail.com>
2025-01-13 23:06:18 +0100

    Database ,App.config & Connection string added. Connection to DB successful.

--- Employee Management/MainWindow.xaml.cs
+++ Employee Management/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using Employee_Management.ViewModel;
+using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Employee_Management
 {
@@ -16,9 +10,52 @@ namespace Employee_Management
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+        //private ObservableCollection<Position> _positions;
+
+        //public ObservableCollection<Position> Positions
+        //{
+        //    get { return _positions; }
+        //    set { _positions = value; }
+        //}
+
+
         public MainWindow()
         {
+            //DataContext = this;
+            //_positions = new ObservableCollection<Position>();
             InitializeComponent();
+            MainWindowVM vm = new MainWindowVM();
+            DataContext = vm;
+        }
+
+        //private void showPositions()
+        //{
+        //    string sql_query = "Select * from positions";
+
+        //    using (IDbConnection connection = new SQLiteConnection(_connectionString))
+        //    {
+        //        List<Position> positions = connection.Query<Position>(sql_query).ToList();
+                
+        //        //foreach (Position position in positions)
+        //        //{
+        //        //    MessageBox.Show($"ID : {position.Position_ID} \nPosition: {position.Position_description}");
+        //        //}
+
+                
+        //        //MessageBox.Show("Positions loaded");
+        //    }
+        //}
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button has been pressed.");
         }
     }
 }
---
+++ Employee Management/model/Position.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+
+namespace Employee_Management.Model
+{
+    /// <summary>
+    /// Defines the positions available for employees
+    /// </summary>
+    public class Position
+    {
+        public int Position_ID { get; init; }
+        public string? Position_description { get; set; }
+
+        //Default constructor for Dapper, no parameters
+        public Position(){}
+
+        public Position(string position_description)
+        {
+            //this.Position_ID = position_ID;
+            this.Position_description = position_description;
+        }
+
+        
+    }
+}
